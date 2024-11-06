7ee3d903031d8fb5d09db3a9543656e90e1a80c4
Muhammad Rendra <mnrendra@users.noreply.github.com>
2024-11-07 01:04:20 +0700

    refactor: update skeleton

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,8 +1,10 @@
+import type Bundle from './Bundle'
 import type Mode from './Mode'
 import type Options from './Options'
 import type Plugin from './Plugin'
 
 export type {
+  Bundle,
   Mode,
   Options,
   Plugin
